63649d64d39073bec52e3ded3fcc66f3f72f7bf4
Abhishek Yadav <41482390+Abhi181818@users.noreply.github.com>
2024-08-28 10:24:19 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/capstonedb/CapstonedbApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstonedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonedbApplication.class, args);
+	}
+
+}
